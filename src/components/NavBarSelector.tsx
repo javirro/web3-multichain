ad8af418bd2aea770bfaf2f53ec8c4f3ce969cce
idcetim <javirofe96@gmail.com>
2023-01-08 18:29:21 +0100

    Add navbar selector with redux

---
+++ src/components/NavBarSelector.tsx
@@ -0,0 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { changeChain } from '../store/chainSlice'
+const NavBarSelector = () => {
+
+  const chain: string = useSelector((s: any) => s.chain)
+  const dispatch = useDispatch()
+
+  const handleChainChange = (ev: any) => { dispatch(changeChain(ev.target.value)) }
+  return (
+    <div className="select-container">
+      <select className="select-chain" onChange={handleChainChange} defaultValue={chain}>
+        <option value={'eth'}>Ethereum</option>
+        <option value={'bnb'}>Binance smart chain</option>
+        <option value={'polygon'}>Polygon</option>
+        <option value={'ftm'}>Fantom</option>
+      </select>
+    </div>
+  )
+}
+
+export default NavBarSelector
--- src/routes/Navbar.tsx
+++ src/routes/Navbar.tsx
@@ -1,23 +1,14 @@
 import '../styles/navbar.css'
 import { NavLink } from 'react-router-dom'
-import { useSelector } from "react-redux"
+import NavBarSelector from '../components/NavBarSelector'
 
 const NavBar = () => {
-  const chain = useSelector((s: any) => s.chain)
-  console.log(chain)
+
   return (
   <div className="navbar-container">
     <NavLink to="/blockchain" className="navbar-link" >Blockchain</NavLink>
     <NavLink to="/tx" className="navbar-link"> Tx </NavLink>
-    <div className="select-container">
-      <select className="select-chain">
-        <option>Ethereum</option>
-        <option>Binance smart chain</option>
-        <option>Polygon</option>
-        <option>Fantom</option>
-      </select>
-
-    </div>
+    <NavBarSelector />
   </div>)
 }
 
